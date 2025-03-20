59597356777626fbc02687520e8f5cb5519bc27b
Désiré NUENTSA <156859724+dnuentsa@users.noreply.github.com>
2025-03-20 14:27:21 +0000

    Upate the description of the project

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # my-first-analytics-project
+This is a project to test streamlit for my data application
---
+++ app.py
@@ -0,0 +1,4 @@
+import streamlit as st 
+
+st.title("My first data application")
+st.write("This is a content to start with")
